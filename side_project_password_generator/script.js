85b54d8d6b1b82597ac3d4a2f8f35db81284e376
R0b3rtG <robertiancu24@gmail.com>
2022-01-13 22:21:40 +0200

    added password generator project

---
+++ side_project_password_generator/script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.btn');
+const passwordField = document.querySelector('.password');
+
+btn.onclick = generatePassword;
+
+function generatePassword(){
+    let password = '';
+    for(let i = 1; i <= 15; i++){
+        const rand = parseInt(Math.random()*122+48);
+        if((rand > 47 && rand < 58) || (rand > 64 && rand < 91) || (rand > 96 && rand < 123)){
+            const char = String.fromCharCode(rand);
+            password += char;
+        } else {
+            i--;
+        }
+    }
+    passwordField.textContent = password;
+}
