8e376d0f1bcdbf663b5aa8b29e8256bac87ed2ba
R0b3rtG <robertiancu24@gmail.com>
2022-02-05 15:16:59 +0200

    updated js for border-radius preview

--- 01_border_radius_preview/app.js
+++ 01_border_radius_preview/app.js
@@ -1,4 +1,3 @@
-const slider = document.querySelectorAll('.slider');
 const slider1 = document.querySelector('.slider1');
 const slider2 = document.querySelector('.slider2');
 const slider3 = document.querySelector('.slider3');
@@ -21,46 +20,41 @@ function borderChange() {
 }
 
 copyBtn.addEventListener('click', copyText);
-
 function copyText() {
 	text.select();
 	text.setSelectionRange(0, 99999);
 	navigator.clipboard.writeText(text.value);
 }
 
+let s1, s2, s3, s4;
+const keepSliderValues = () => {
+	s1 = slider1.value;
+	s2 = slider2.value;
+	s3 = slider3.value;
+	s4 = slider4.value;
+};
+
+const restoreSliderValues = () => {
+	slider1.value = s1;
+	slider2.value = s2;
+	slider3.value = s3;
+	slider4.value = s4;
+	box.style.borderRadius = `${s1}% ${s2}% ${s3}% ${s4}%`;
+};
+
 optionBMO.addEventListener('click', function () {
-	const s1 = slider1.value;
-	const s2 = slider2.value;
-	const s3 = slider3.value;
-	const s4 = slider4.value;
+	keepSliderValues();
 	box.setAttribute('style', "background-image: url('./img/wallpaper1.png')");
-	slider1.value = s1;
-	slider2.value = s2;
-	slider3.value = s3;
-	slider4.value = s4;
-	box.style.borderRadius = `${slider1.value}% ${slider2.value}% ${slider3.value}% ${slider4.value}%`;
+	restoreSliderValues();
 });
+
 optionFinn.addEventListener('click', function () {
-	const s1 = slider1.value;
-	const s2 = slider2.value;
-	const s3 = slider3.value;
-	const s4 = slider4.value;
+	keepSliderValues();
 	box.setAttribute('style', "background-image: url('./img/wallpaper2.jpg')");
-	slider1.value = s1;
-	slider2.value = s2;
-	slider3.value = s3;
-	slider4.value = s4;
-	box.style.borderRadius = `${slider1.value}% ${slider2.value}% ${slider3.value}% ${slider4.value}%`;
+	restoreSliderValues();
 });
 optionJake.addEventListener('click', function () {
-	const s1 = slider1.value;
-	const s2 = slider2.value;
-	const s3 = slider3.value;
-	const s4 = slider4.value;
+	keepSliderValues();
 	box.setAttribute('style', "background-image: url('./img/wallpaper3.png')");
-	slider1.value = s1;
-	slider2.value = s2;
-	slider3.value = s3;
-	slider4.value = s4;
-	box.style.borderRadius = `${slider1.value}% ${slider2.value}% ${slider3.value}% ${slider4.value}%`;
+	restoreSliderValues();
 });
